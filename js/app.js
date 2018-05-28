5b476d5d10b7e0c59bb0a9089c522c5be9d127a1
kmoroz <ksmorozova@hotmail.com>
2018-05-28 22:14:50 +0100

    add interactiveness: cards match/do not match :flower_playing_cards:

--- js/app.js
+++ js/app.js
@@ -3,6 +3,34 @@
  */
 
 
+const MAXOPENCARDS = 2;
+const NUMOFSTARS = 3
+const CARDLIST = 
+	[ 
+		{class:"road", character: "道", answer: "road"}, 
+		{class: "moon", character:"月", answer: "moon"},
+		{class: "snow", character: "雪", answer: "snow"},
+		{class: "cloud", character:"雲", answer: "cloud"},
+		{class: "home", character:"家", answer: "home"},
+		{class: "alcohol", character:"酒", answer: "alcohol"},
+		{class: "eye", character:"目", answer: "eye"},
+		{class: "image", character:"絵", answer: "image"},
+		{class: "fa-snowflake", type:"fa", answer: "snow"},
+		{class: "fa-road", type:"fa", answer: "road"},
+		{class: "fa-image", type:"fa", answer: "image"},
+		{class: "fa-glass-martini", type:"fa", answer: "alcohol"},
+		{class: "fa-cloud", type:"fa", answer: "cloud"},
+		{class: "fa-eye", type:"fa", answer: "eye"},
+		{class: "fa-moon", type:"fa", answer: "moon"},
+		{class: "fa-home", type:"fa", answer: "home"}		
+	];
+
+
+var shuffledCards;
+
+// var kanjiCardsList = ["road", "moon", "snow", "cloud", "home", "alcohol", "eye", "image"];
+var iconCardsList = ["", "", "", "", "", "", "", ""];
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -24,6 +52,84 @@ function shuffle(array) {
 
     return array;
 }
+
+
+function createCard(){
+	$('.deck').empty();
+	shuffledCards = shuffle(CARDLIST);
+	shuffledCards.forEach(function(card, i){
+		if(card.type == "fa"){
+			$('.deck').append("<li><i id=\"" + i + "\" class=\"card fa " + card.class + "\"></i></li>");
+		}
+
+		else{
+			$('.deck').append("<li><i id=\"" + i + "\" class=\"card " + card.class + "\">" + card.character + "</i></li>");
+		}
+	});
+}
+
+$(".restart").click(function(){
+    createCard();
+});
+
+
+
+var openCards = [];
+
+
+//open card function
+$(document).on("click",".card", function () {
+    clickedCard = $(this);
+    openClickedCard(clickedCard);
+    
+    if(openCards.length === MAXOPENCARDS){
+        setTimeout(resetUmatchedCards, 700);
+    }
+});
+
+function openClickedCard(clickedCard){
+    if (!clickedCard.hasClass("open") && openCards.length < MAXOPENCARDS){
+
+    	// open up card
+        clickedCard.addClass("open");
+
+        // add answer to answerlist
+        var cardIndex = clickedCard.attr("id");
+        var card = shuffledCards[cardIndex];
+
+        openCards.push({answer: card.answer, id: cardIndex});
+
+    if (openCards.length === MAXOPENCARDS){
+		card1 = openCards[0];
+		card2 = openCards[1];
+
+    	if (card1.answer === card2.answer){
+			document.getElementById(card1.id).classList.add("match");
+			document.getElementById(card2.id).classList.add("match");
+
+
+		
+    	}
+    }
+    }     
+}
+
+
+
+function checkCardListForMatches(){
+
+}
+
+function resetUmatchedCards(){
+    $(".card").each(function(){
+        if (!$(this).hasClass("match")){
+            $(this).removeClass("open");
+        }
+    });
+
+    openCards = [];
+}
+
 
 
 /*
